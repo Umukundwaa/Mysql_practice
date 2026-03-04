60f327ba39d3c160865c65972891cfebba149101
umukundwaa <a.umukundwa1@alustudent.com>
2026-03-04 10:22:30 -0800

    implement manipulate_data file

---
+++ manipulate_data.sql
@@ -0,0 +1,28 @@
+UPDATE Books
+SET price = price + 5
+WHERE title = 'The Silent Patient';
+
+
+UPDATE Books
+SET stock_quantity = 25
+WHERE title = 'Atomic Habits';
+
+
+DELETE FROM Customers
+WHERE email = 'john.doe@example.com';
+
+
+DELETE FROM Orders
+WHERE order_id = 3;
+
+
+SELECT * FROM Books
+WHERE genre = 'Fiction';
+
+
+SELECT * FROM Orders
+WHERE total_amount > 50;
+
+
+SELECT * FROM Customers
+WHERE last_name ILIKE '%son%';
